8cd81b629e5ef1fe5792c6e19467f70f0f0b838a
Priscilla <priscillaonil@gmail.com>
2023-12-26 17:39:30 -0300

    style: change backgournd color

--- src/components/Sidebar/styles.tsx
+++ src/components/Sidebar/styles.tsx
@@ -1,9 +1,11 @@
 import styled, { css } from "styled-components";
 
 export const SidebarContainer = styled.div`
-  ${() => css`
+  ${({ theme }) => css`
     display: flex;
     flex-direction: column;
     gap: 4px;
+    background-color: ${theme.colors.neutral[400]};
+    width: 257px;
   `}
 `;
