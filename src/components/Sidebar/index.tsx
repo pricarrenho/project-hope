607b1df0f1cd520bb049d5ba3ceff872720dee8d
Priscilla <priscillaonil@gmail.com>
2024-01-02 17:38:44 -0300

    feat: create sidebar component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,13 +1,159 @@
-import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
+import category from "../../assets/images/category.png";
+import menuStyle from "../../assets/images/menuStyle.png";
+import example from "../../assets/images/example.png";
+import widgets from "../../assets/images/widgets.png";
+import maps from "../../assets/images/maps.png";
+import authentication from "../../assets/images/authentication.png";
+import user from "../../assets/images/user.png";
+import error404 from "../../assets/images/error404.png";
+import error505 from "../../assets/images/error505.png";
+import maintence from "../../assets/images/maintence.png";
+import components from "../../assets/images/components.png";
+import form from "../../assets/images/form.png";
+import table from "../../assets/images/table.png";
+import icons from "../../assets/images/icons.png";
+import { IoIosArrowForward } from "react-icons/io";
+import { FaArrowLeft } from "react-icons/fa";
+
 import * as S from "./styles";
+import { useState } from "react";
+
+const SidebarLibrary = [
+  [
+    { title: "Home" },
+    {
+      name: "Dashboard",
+      icons: category,
+      route: "/",
+    },
+    {
+      name: "Menu Style",
+      icons: menuStyle,
+      route: "/menu-style",
+    },
+  ],
+
+  [
+    { title: "Page" },
+    {
+      name: "Example",
+      icons: example,
+      route: "/",
+    },
+    {
+      name: "Widgets",
+      icons: widgets,
+      route: "/",
+    },
+    {
+      name: "Maps",
+      icons: maps,
+      route: "/",
+    },
+    {
+      name: "Authentication",
+      icons: authentication,
+      route: "/",
+    },
+    {
+      name: "Users",
+      icons: user,
+      route: "/",
+    },
+    {
+      name: "Error 404",
+      icons: error404,
+      route: "/",
+    },
+    {
+      name: "Error 505",
+      icons: error505,
+      route: "/",
+    },
+    {
+      name: "Maintence",
+      icons: maintence,
+      route: "/",
+    },
+  ],
+
+  [
+    { title: "Elements" },
+    {
+      name: "Components",
+      icons: components,
+      route: "/",
+    },
+    {
+      name: "Form",
+      icons: form,
+      route: "/",
+    },
+    {
+      name: "Table",
+      icons: table,
+      route: "/",
+    },
+    {
+      name: "Icons",
+      icons: icons,
+      route: "/",
+    },
+  ],
+];
 
 export const Sidebar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+
+  const handleCloseMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <S.SidebarContainer>
-      <p>Sidebar</p>
+      <S.LogoContainer>
+        <S.LogoImage
+          src={logo}
+          alt="Logo da hope UI. Traços azuis conectados formando uma hashtag "
+        />
 
-      <Link to="/">Home</Link>
-      <Link to="/menu-style">Menu Style</Link>
+        {isMenuOpen && <S.LogoText>Hope UI</S.LogoText>}
+      </S.LogoContainer>
+
+      <S.Btn onClick={handleCloseMenu}>
+        <FaArrowLeft size={18} color="#ffffff" />
+      </S.Btn>
+
+      <S.SidebarContent>
+        {SidebarLibrary.map((item, index) => (
+          <div key={index}>
+            {item.map((value, index) => (
+              <div key={index}>
+                {value.title && isMenuOpen && (
+                  <S.Titles>{value.title}</S.Titles>
+                )}
+
+                <S.SidebarButtonsContainer>
+                  {value.route && (
+                    <S.LinkContainer to={value.route}>
+                      <S.LinkLeftContent>
+                        <S.LinkImage src={value.icons} />
+
+                        {isMenuOpen && <S.LinkTitle>{value.name}</S.LinkTitle>}
+                      </S.LinkLeftContent>
+
+                      {isMenuOpen && <IoIosArrowForward color="#8A92A6" />}
+                    </S.LinkContainer>
+                  )}
+                </S.SidebarButtonsContainer>
+              </div>
+            ))}
+
+            <S.Divisor />
+          </div>
+        ))}
+      </S.SidebarContent>
     </S.SidebarContainer>
   );
 };
--- src/components/Sidebar/styles.tsx
+++ src/components/Sidebar/styles.tsx
@@ -1,11 +1,126 @@
 import styled, { css } from "styled-components";
+import { Link } from "react-router-dom";
 
 export const SidebarContainer = styled.div`
   ${({ theme }) => css`
     display: flex;
     flex-direction: column;
-    gap: 4px;
+    position: relative;
     background-color: ${theme.colors.neutral[400]};
-    width: 257px;
+    filter: drop-shadow(8px 1px 100px ${theme.colors.neutral[300]});
+    transition: max-width 0ms.2s;
+  `}
+`;
+
+export const LogoContainer = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 16px;
+    border-bottom: 2px solid ${theme.colors.neutral[300]};
+    padding: 16px;
+    height: 40px;
+  `}
+`;
+
+export const LogoImage = styled.img`
+  ${() => css`
+    width: 28px;
+    height: 28px;
+  `}
+`;
+
+export const LogoText = styled.h1`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.lg};
+    font-weight: 500;
+    color: ${theme.colors.white};
+    line-height: 130%;
+  `}
+`;
+
+export const Btn = styled.button`
+  ${({ theme }) => css`
+    position: absolute;
+    right: -16px;
+    top: 22px;
+
+    display: flex;
+    justify-content: center;
+    padding: 6px;
+    border-radius: 33px;
+    border: none;
+    background-color: ${theme.colors.blue[400]};
+    transition: 0.2s;
+
+    &:hover {
+      background-color: ${theme.colors.blue[500]};
+    }
+  `}
+`;
+
+export const SidebarContent = styled.div`
+  ${() => css`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    padding: 24px;
+  `}
+`;
+
+export const Titles = styled.h2`
+  ${({ theme }) => css`
+    color: ${theme.colors.neutral[100]};
+    font-size: ${theme.font.sizes.md};
+    font-weight: 600;
+    line-height: 175%;
+  `}
+`;
+
+export const SidebarButtonsContainer = styled.div`
+  ${() => css`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  `}
+`;
+
+export const LinkContainer = styled(Link)`
+  ${() => css`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    padding: 8px 0px;
+  `}
+`;
+
+export const LinkLeftContent = styled.div`
+  ${() => css`
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  `}
+`;
+
+export const LinkImage = styled.img`
+  ${() => css``}
+`;
+
+export const LinkTitle = styled.p`
+  ${({ theme }) => css`
+    color: ${theme.colors.neutral[200]};
+    font-size: ${theme.font.sizes.md};
+    font-weight: 400;
+    line-height: 175%;
+  `}
+`;
+
+export const Divisor = styled.div`
+  ${({ theme }) => css`
+    background: ${theme.colors.neutral[250]};
+    width: 100%;
+    height: 1px;
   `}
 `;
